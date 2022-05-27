d6d07b9fc44c016cc0432f95d9bcbcf8dc6a4310
Kadath <dawidcichy34@gmail.com>
2022-05-27 23:34:50 +0200

    Add project files.

---
+++ MusicApp/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MusicApp
+{
+    public class Album
+    {
+        private int Id { get; set; }
+        public string Name { get; set; }
+        public string Band { get; set; }
+        public int Year { get; set; }
+        public ImageSource Cover { get; set; }
+
+    }
+}
---
+++ MusicApp/Models/Songs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Models
+{
+    public class Songs
+    {
+        private int Id { get; set; }
+        public string Name { get; set; }
+        public string Band { get; set; }
+        public int Year { get; set; }
+        public string Album { get; set; }
+    }
+}
